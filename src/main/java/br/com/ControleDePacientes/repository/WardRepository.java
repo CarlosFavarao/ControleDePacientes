4bcd4d8955c60493f84b82b4993668ae9cc5d969
Carlos Favarão <carlosfavarao12@gmail.com>
2025-05-29 17:51:15 -0300

    Iniciei a parte de ver os status de camas

--- src/main/java/br/com/ControleDePacientes/repository/WardRepository.java
+++ src/main/java/br/com/ControleDePacientes/repository/WardRepository.java
@@ -1,9 +1,25 @@
 package br.com.ControleDePacientes.repository;
 
+import br.com.ControleDePacientes.dto.SpecialtyBedStatsDTO;
 import br.com.ControleDePacientes.model.WardModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WardRepository extends JpaRepository<WardModel, Long> {
+
+    @Query(value = "SELECT" +
+                    "w.specialty AS specialty, " +
+                    "CAST(COUNT(b.id) AS BIGINT) AS totalBeds, " +                                             //Estudar depois!
+                    "CAST(SUM(CASE WHEN b.status = 'AVAILABLE' THEN 1 ELSE 0 END) AS BIGINT) AS freeBeds,  " + //Aprender melhor sobre as queries, ainda não consigo fazer elas sozinho.
+                    "CAST(SUM(CASE WHEN b.status = 'OCUPIED' THEN 1 ELSE 0 END) AS BIGINT) AS occupiedBeds " +
+                    "FROM wards w " +
+                    "JOIN rooms r ON w.id = r.ward_id " +
+                    "JOIN beds b ON r.id = b.room_id " +
+                    "GROUP BY w.specialty",
+                    nativeQuery = true)
+    List<SpecialtyBedStatsDTO> getBedStatsBySpecialty();
 }
