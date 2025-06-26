ab235fbfdd6c4b0ae26699b9ca8511320c2f160c
Gabriel <gabrielfg12345@gmail.com>
2025-06-26 17:58:05 -0300

    Criação das classes de visitante e log de visitas

--- src/main/java/br/com/ControleDePacientes/model/VisitorModel.java
+++ src/main/java/br/com/ControleDePacientes/model/VisitorModel.java
@@ -1,4 +1,23 @@
 package br.com.ControleDePacientes.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "visitors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VisitorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String document;
 }
