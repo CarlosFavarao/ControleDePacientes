c417a88b1564080259ee1dc05c40f1a19043222e
Matheus Pablo <mathpablo1@gmail.com>
2025-06-27 14:21:21 -0300

    Criação da classe exame, e dos enus exames e tipos exames

---
+++ src/main/java/br/com/ControleDePacientes/enums/ExamStatus.java
@@ -0,0 +1,7 @@
+package br.com.ControleDePacientes.enums;
+
+public enum ExamStatus {
+    SCHEDULED,
+    ACCOMPLISHED,
+    CANCELED
+}
--- src/main/java/br/com/ControleDePacientes/enums/ExamType.java
+++ src/main/java/br/com/ControleDePacientes/enums/ExamType.java
@@ -1,4 +1,10 @@
 package br.com.ControleDePacientes.enums;
 
 public enum ExamType {
+    LABORATORY,
+    IMAGING,
+    FUNCTIONAL,
+    ENDOSCOPIC,
+    GENETIC_MOLECULAR,
+    GYNECOLOGICAL_UROLOGICAL
 }
