eb93302d919057e980e2ab1e5d46331d1fb86efe
LucasFerrari14 <lucasferrarisoares@Gmail.com>
2025-06-30 15:39:05 -0300

    Criado Histórico de médicos por internação.

---
+++ src/main/java/br/com/ControleDePacientes/model/DoctorHistoryModel.java
@@ -0,0 +1,36 @@
+package br.com.ControleDePacientes.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "doctor_history")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoctorHistoryModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "admission_id", nullable = false)
+    private AdmissionLogModel admission;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private DoctorModel doctor;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+}
